36d0db045a668f008b55326a8e0d1ac00609a170
Anton Roback <113305944+KarateKlas@users.noreply.github.com>
2026-05-25 11:37:52 +0200

    Revert "enjoy"

--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@
                 Console.WriteLine("=== Start av programmet ===");
                 Console.WriteLine("Christofer was here");
 
-                Console.WriteLine(var);
                 // Exempel 1: try-catch-finally
                 try
                 {
